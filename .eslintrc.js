68b7d03c9d48fa98d99f0b8a42ad4c4d07bcbb41
michellepennat <mp.design.apps@gmail.com>
2021-01-06 15:55:17 -0500

    eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+  },
 };
