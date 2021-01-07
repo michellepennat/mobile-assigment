3a3f3e2c3ce69692f9d63b9e04495683ab801b57
michellepennat <mp.design.apps@gmail.com>
2021-01-06 19:26:15 -0500

    refactor(Detail): add style

--- src/scenes/Detail/Detail.js
+++ src/scenes/Detail/Detail.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+
+import {Container, Header} from 'native-base';
+import {View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import styles from './Detail.style';
+import Card from '../../components/CardDetail/CardDetail';
 
 const Detail = () => (
-  <View>
-    <Text>Hola</Text>
-  </View>
+  <SafeAreaView>
+    <Header style={[styles.header]}>
+      <TouchableOpacity>
+        <Text>Atrás</Text>
+      </TouchableOpacity>
+      <Text style={[styles.textHeader]}>Detalle</Text>
+    </Header>
+    <View style={[styles.container]}>
+      <Card />
+    </View>
+  </SafeAreaView>
 );
 export default Detail;
--- src/scenes/Detail/Detail.style.js
+++ src/scenes/Detail/Detail.style.js
@@ -1,6 +1,31 @@
 import {StyleSheet, Dimensions} from 'react-native';
-import {PRIMARY_COLOR, WHITE_COLOR} from './_common';
+import {
+  BACKGROUND_COLOR,
+  PRIMARY_COLOR,
+  WHITE_COLOR,
+} from '../../assets/style/_common';
 
 var {height} = Dimensions.get('window');
 
-export const style = StyleSheet.create({});
+export default style = StyleSheet.create({
+  container: {
+    backgroundColor: BACKGROUND_COLOR,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: 30,
+    paddingHorizontal: 20,
+  },
+  header: {
+    backgroundColor: PRIMARY_COLOR,
+    display: 'flex',
+    alignItems: 'center',
+    alignItems: 'center',
+  },
+  textHeader: {
+    color: WHITE_COLOR,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
