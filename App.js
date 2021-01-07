7c1c69c530de0f899963ea92d6d22e8663d767b3
michellepennat <mp.design.apps@gmail.com>
2021-01-06 22:15:05 -0500

    feat(navigation): Setting routes

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, Text} from 'react-native';
 import {View} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {BACKGROUND_COLOR, PRIMARY_COLOR} from './src/assets/style/_common';
 
 import Detail from './src/scenes/Detail/Detail';
 import Feed from './src/scenes/Feed/Feed';
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <View style={{backgroundColor: BACKGROUND_COLOR}}>
-      <StatusBar backgroundColor={PRIMARY_COLOR} barStyle="dark-content" />
-      {/* <Feed /> */}
-      <Detail />
-    </View>
+    // <View style={{backgroundColor: BACKGROUND_COLOR}}>
+    //   <StatusBar backgroundColor={PRIMARY_COLOR} barStyle="dark-content" />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="feed">
+        <Stack.Screen name="feed" component={Feed} />
+        <Stack.Screen name="detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </View>
   );
 };
 
